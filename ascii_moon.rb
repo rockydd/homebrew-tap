0a717050549bb6bd7bb63dde30739c3d26607f89
rockydd <64197+rockydd@users.noreply.github.com>
2025-12-09 23:33:24 -0800

    Update ascii_moon to v0.2.0

--- ascii_moon.rb
+++ ascii_moon.rb
@@ -1,8 +1,8 @@
 class AsciiMoon < Formula
   desc "A TUI to show the moon phase"
   homepage "https://github.com/rockydd/ascii_moon"
-  url "https://github.com/rockydd/ascii_moon/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7fcbed8d8012faf74995f912ee60ab97433ee3f2ad76dae858f437801e6c9604"
+  url "https://github.com/rockydd/ascii_moon/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "be5d1ad3a5bfad11fedc093b256d2153e93de084969d45496f2a5f60da712077"
   license "MIT"
 
   depends_on "rust" => :build
